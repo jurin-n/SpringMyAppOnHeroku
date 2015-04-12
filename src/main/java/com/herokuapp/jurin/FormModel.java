25078236390bc6dee9f27bec071f3401ab1b6d95
jun nakano <jurin.nakano@gmail.com>
2015-04-12 14:43:54 +0900

    my first commit.

---
+++ src/main/java/com/herokuapp/jurin/FormModel.java
@@ -0,0 +1,13 @@
+package com.herokuapp.jurin;
+
+public class FormModel {
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
